8520d4bb6cc68515b7a2cc01d0dcd63ce86af612
Jay-Mithani <jayvmithani@gmail.com>
2019-03-18 15:25:31 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import PredictionForm from './Components/PredictionForm';
+import Main from './pages/Main';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PredictionForm></PredictionForm>
-      </div>
+    	<Main/>
     );
   }
 }
--- server.js
+++ server.js
@@ -10,16 +10,19 @@ app.use(bodyParser.json());
 
 let port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "client", "build")))
-
 app.post('/api', async(req,res) => {
 	// parse req.body and do the apropriate backend things
 	// for example req.body.type == 'login' then do login things
-})
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
+
+// app.use((req, res, next) => {
+// 	const test = /\?[^]*\//.test(req.url);
+// 	if (req.url.substr(-1) === '/' && req.url.length > 1 && !test)
+// 		res.redirect(301, req.url.slice(0, -1));
+// 	else
+// 		next();
+// });
+app.use(express.static(path.join(__dirname, "client", "static")));
 
 console.log("Server started on port " + port);
 
