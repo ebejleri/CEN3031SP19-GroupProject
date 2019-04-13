8dd159f6ffdb408d6b272e4e5a86075e9602e775
ebejleri <eric.bejleri@ufl.edu>
2019-04-13 15:31:53 -0400

    added in payment needed before requesting appointment

--- client/static/user/js/calendarRequest.js
+++ client/static/user/js/calendarRequest.js
@@ -4,17 +4,23 @@ const calHandler = (e) => {
 	const time = $('#selectTime').val();
 	console.log(date + ",  " + time);
 	const account = JSON.parse(window.sessionStorage.getItem('account'));
-	if (date == null || date.length < 3) {
-		alert("Please put in valid date!");
+	if(!account.pending_payment){
+		if (date == null || date.length < 3) {
+			alert("Please put in valid date!");
+		}
+		else {
+			$('#name').val("");
+			$("selectTime").val(1);
+			$.post('../account/appointment', {
+				date: date,
+				time: time,
+				email: account ? account.email : 'test@gmail.com',
+			})
+			alert("Request Sent!");
+		}
 	}
-	else {
-		$('#name').val("");
-		$("selectTime").val(1);
-		$.post('../account/appointment', {
-			date: date,
-			time: time,
-			email: account ? account.email : 'test@gmail.com',
-		})
+	else{
+		alert("Payment is Required before Requesting a Consultation");
 	}
 }
 $('#sendMessageButton').click(calHandler);
